1ed0911f6f2c3b2ed3952530a17834367827a035
bzreinhardt <bzreinhardt@gmail.com>
2018-10-24 16:32:43 -0700

    hotfix null:

---
+++ app/special_labels.py
@@ -0,0 +1,15 @@
+SPECIAL_LABELS = [
+  "Looking For",
+  "Has",
+  "Know Via",
+  "Loc",
+  "From",
+  "Alias",
+  "Relationship",
+  "Talk About",
+  "Married To",
+  "Engaged To",
+  "Dating",
+  "Friends With",
+  "Email"
+]
